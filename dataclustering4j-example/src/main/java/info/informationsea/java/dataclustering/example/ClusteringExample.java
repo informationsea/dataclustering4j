a9aaa85346cfe27966c136c609c60581407bc970
OKAMURA Yasunobu <okamura@informationsea.info>
2014-11-27 17:29:40 +0900

    add License to ClusteringExample.java

--- dataclustering4j-example/src/main/java/info/informationsea/java/dataclustering/example/ClusteringExample.java
+++ dataclustering4j-example/src/main/java/info/informationsea/java/dataclustering/example/ClusteringExample.java
@@ -1,3 +1,21 @@
+/**
+ *  dataclustering4j
+ *  Copyright (C) 2014 Yasunobu OKAMURA
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package info.informationsea.java.dataclustering.example;
 
 import info.informationsea.java.dataclustering.clustering.ClusterNode;
@@ -11,9 +29,6 @@ import info.informationsea.java.dataclustering.matrix.Matrix;
 import java.io.FileReader;
 import java.io.IOException;
 
-/**
- * Created by yasu on 14/11/27.
- */
 public class ClusteringExample {
     public static void main(String[] argv) {
         if (argv.length != 1) {
